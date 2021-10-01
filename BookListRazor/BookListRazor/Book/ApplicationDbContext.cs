accd9d1f6bd3c29d7f6e8a0285a4a0ef9fa9277a
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-10-01 17:12:30 +0000

    Added ApplicationDbContext to configure ECF services. Added DbContext in Startup to nclude EF inside the configuration pipeline. Then Push to database.

---
+++ BookListRazor/BookListRazor/Book/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Book
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; } // To add a model the database inside dbcontext we need this as an entry.
+    }
+}
