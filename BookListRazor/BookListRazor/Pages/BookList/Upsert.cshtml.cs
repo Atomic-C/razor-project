df5e09ec82ec064a62605ec55906912a675ea5ec
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-10-07 18:14:15 +0000

    Created Upsert Handler, should unite Edit and Create functionality upon different needs.

---
+++ BookListRazor/BookListRazor/Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class UpsertModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public UpsertModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id) // Nullable integer, in case we are Creating, because ID will not exist...
+        {
+            Book = new Book();
+            if (id == null)
+            {   // Create
+                return Page();
+            }
+
+                Book = await _db.Book.FirstOrDefaultAsync(u=>u.Id ==id); // Read more on lambda expressions please
+                //Book = await _db.Book.FindAsync(id);
+            
+            if (Book == null)
+            {   // Update
+                return NotFound();
+            }
+            return Page();
+        }
+        public async Task<IActionResult> OnPost(Book bookObj)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (Book.Id == 0)
+                {
+                    _db.Book.Add(Book);
+                }
+                else
+                {
+                    _db.Book.Update(Book); //  Here we do Update when we want to update EVERY PROPERTY of the book. Or like in edit page, we can edit properties individually.
+                }
+                await _db.SaveChangesAsync(); // Now it's saved to the database. Only thing I don't get is this asyncronous stuff.
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return RedirectToPage();
+            }
+        }
+    }
+}
